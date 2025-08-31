1c8550887e085ff31e30d143cdea1d78e4816f02
Shivam Hirani <shivamh767@gmail.com>
2025-08-31 20:51:06 +0530

    Add files via upload

---
+++ RESUME.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main ()
+{
+   clrscr();
+
+   printf("Name: Hirani Shivam R.\n");
+   printf("Age:21\n");
+   printf("B.C.A Sem 1\n");
+
+
+   getch();
+
+}
+
+
---
+++ Space valo program.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main ()
+{
+
+     clrscr();
+     printf("*\t*\t*\t*\t*\t*\t*\t*\t*\t*\n");
+     printf("*\t\t\t\tHi Brother\t\t\t\t*\n");
+     printf("*\t\t\t\t Welcome \t\t\t\t*\n");
+     printf("*\t\t\t\t Wake up \t\t\t\t*\n");
+     printf("*\t\t\t\t Wake up \t\t\t\t*\n");
+     printf("*\t\t\t\t Wake up \t\t\t\t*\n");
+     printf("*\t*\t*\t*\t*\t*\t*\t*\t*\t*\n");
+     getch ();
+
+}
